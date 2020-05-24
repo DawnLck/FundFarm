ac83074f8ff4b670b3d89556c6a08b5e7d49e04b
LiangChaokai <youchoudeyu@qq.com>
2020-05-24 22:37:14 +0800

    🐛 fix: 修复全局变量导入出错的一系列问题

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,33 @@
+const path = require("path");
+
 module.exports = {
   pages: {
+    // popup - 弹出框的页面构建
     popup: {
       template: "public/browser-extension.html",
       entry: "./src/popup/main.ts",
-      title: "Popup"
-    }
+      title: "Popup",
+    },
   },
   pluginOptions: {
     browserExtension: {
       componentOptions: {
         background: {
-          entry: "src/background.ts"
-        }
-      }
-    }
-  }
+          entry: "src/background.ts",
+        },
+      },
+    },
+    "style-resources-loader": {
+      preProcessor: "less",
+      patterns: [path.resolve(__dirname, "./src/common/less/*.less")],
+    },
+  },
+  chainWebpack: (config) => {
+    const types = ["vue-modules", "vue", "normal-modules", "normal"];
+    types.forEach((type) =>
+      addStyleResource(config.module.rule("stylus").oneOf(type))
+    );
+  },
 };
+
+function addStyleResource() {}
